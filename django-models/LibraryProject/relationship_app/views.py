5c31bcee4f5286a6115081695cd9dcb794a697b8
Benjamin Kesse Ampadu <ampadubenjaminkesse@gmail.com>
2025-11-06 10:40:15 +0000

    Relationship App

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Book
+from django.views.generic.detail import DetailView
 
 # Create your views here.
+def booklist(request):
+    books = Book.objects.all()
+    context = {'book_list': books}
+    return render(request, 'relationship_app/booklist.html',context)
