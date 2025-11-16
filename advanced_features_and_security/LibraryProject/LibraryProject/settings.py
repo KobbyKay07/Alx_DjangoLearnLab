a1ef72ea64104efb9b9966cc43690aa8926c5c27
Benjamin Kesse Ampadu <ampadubenjaminkesse@gmail.com>
2025-11-16 14:34:36 +0000

    Django advanced features and security

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -57,6 +57,10 @@ CSP_FRAME_ANCESTORS = ("'none'",)
 # SECURITY: Redirect all HTTP traffic to HTTPS
 SECURE_SSL_REDIRECT = True
 
+# Tells Django to trust the X-Forwarded-Proto header from the proxy
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+
 # HTTP Strict Transport Security (HSTS)
 # Be careful: enabling HSTS tells browsers to always use HTTPS for your domain.
 # Don't enable this until HTTPS works reliably for your site.
