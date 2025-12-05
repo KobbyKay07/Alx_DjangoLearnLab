85388c9b5a71cc592cb9ae99c6d8461e64586fbf
Benjamin Kesse Ampadu <ampadubenjaminkesse@gmail.com>
2025-12-05 06:16:47 +0000

    Creating Blog Post Management Features

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,77 +2,117 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .forms import UserProfileForm
+
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from .permissions import IsAuthor
+
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from .forms import UserProfileForm
 from .models import Post
 from .serializers import PostSerializer
-from django.views.generic import CreateView
+from .permissions import IsAuthor
 
 
-# Create your views here.
-
+# Authentication & Profile Views
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "blog/register.html"
 
+
 @login_required
 def profile(request):
-    """
-    Displays and allows editing of the user's profile.
-    GET: Shows the current profile details.
-    POST: Updates the user's profile if form is valid.
-    """
     user = request.user
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=user)
         if form.is_valid():
             form.save()
-            return redirect('profile')  # redirect to the same profile page after update
+            return redirect('profile')
     else:
         form = UserProfileForm(instance=user)
 
-    context = {
-        'form': form,
-    }
-    return render(request, 'profile.html', context)
+    return render(request, 'profile.html', {'form': form})
 
+
+
+# DRF API Views (JSON)
 class List_View(generics.ListAPIView):
-    '''
-    Displays a list of all Posts.
-    '''
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
 
 class Detail_View(generics.RetrieveAPIView):
-    '''
-    Displays details of a specific Post.
-    '''
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
 
 class Create_View(generics.CreateAPIView):
-    '''
-    Allows creation of a new Post.
-    '''
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated] # Only authenticated users can create posts
+    permission_classes = [IsAuthenticated]
+
 
 class Update_View(generics.UpdateAPIView):
-    '''
-    Allows updating an existing Post.
-    '''
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated, IsAuthor] # Only authors users can update posts
+    permission_classes = [IsAuthenticated, IsAuthor]
+
 
 class Delete_View(generics.DestroyAPIView):
-    '''
-    Allows deletion of a Post.
-    '''
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated, IsAuthor] # Only authors users can delete posts
+    permission_classes = [IsAuthenticated, IsAuthor]
+
+
+# Template-Based Views for Blog Posts
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ["title", "content"]
+    template_name = "blog/post_form.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form_title"] = "Create Post"
+        return context
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ["title", "content"]
+    template_name = "blog/post_form.html"
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form_title"] = "Edit Post"
+        return context
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = "blog/post_confirm_delete.html"
+    success_url = reverse_lazy("posts")
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
