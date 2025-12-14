bcb4807bc00d71811a2028538e3aaf77b1a7ca4f
Benjamin Kesse Ampadu <ampadubenjaminkesse@gmail.com>
2025-12-14 10:52:23 +0000

    Added Urls for follow and unfollow

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -17,7 +17,5 @@ urlpatterns = [
         CommentViewSet.as_view({"get": "retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"}),
         name="comment-detail"
     ),
-    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow-user'),
-    path('unfollow/<int:user_id>/', FollowUserView.as_view(), name='unfollow-user'),
      path('feed/', FeedView.as_view(), name='user_feed')
 ]
